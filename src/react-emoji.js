a86e708912a41903ec017fabbf32092faeb846c7
Brent Luehr <brent@nerdcon.com>
2018-05-25 10:38:03 -0700

    Use DoSomeGood CDN for twemoji

--- src/react-emoji.js
+++ src/react-emoji.js
@@ -1,72 +1,91 @@
-import React              from 'react';
-import annotations        from 'emoji-annotation-to-unicode';
-import emoticons          from 'emoji-emoticon-to-unicode';
-import escapeStringRegexp from 'escape-string-regexp';
-import assign             from 'object-assign';
-import compact            from 'lodash.compact';
+import React from "react";
+import annotations from "emoji-annotation-to-unicode";
+import emoticons from "emoji-emoticon-to-unicode";
+import escapeStringRegexp from "escape-string-regexp";
+import assign from "object-assign";
+import compact from "lodash.compact";
 
 let ReactEmoji = () => {
-  let getEscapedKeys = (hash) => {
+  let getEscapedKeys = hash => {
     return Object.keys(hash)
       .map(x => escapeStringRegexp(x))
-      .join('|');
+      .join("|");
   };
 
-  let buildOptions = (options) => {
+  let buildOptions = options => {
     let hash = {
       useEmoticon: options.useEmoticon === false ? false : true,
-      emojiType: options.emojiType || 'twemoji',
-      host: options.host || '',
-      path: options.path || '',
-      ext: options.ext || 'svg',
+      emojiType: options.emojiType || "twemoji",
+      host: options.host || "",
+      path: options.path || "",
+      ext: options.ext || "svg",
       singleEmoji: options.singleEmoji || false,
       strict: options.strict || false
     };
-    hash.attributes = assign({width: '20px', height: '20px'}, options.attributes);
+    hash.attributes = assign(
+      { width: "20px", height: "20px" },
+      options.attributes
+    );
     return hash;
   };
 
   // Use negated lookahead for `:/`, refs: https://github.com/banyan/react-emoji/issues/1
-  let specialEmoticons = {':/': '1f615'};
+  let specialEmoticons = { ":/": "1f615" };
   let specialEmoticonsRegex = "\\:\\/(?!\\/)";
 
   const emojiWithEmoticons = {
-    delimiter: new RegExp(`(:(?:${getEscapedKeys(annotations)}):|${getEscapedKeys(emoticons)}|${specialEmoticonsRegex})`, 'g'),
+    delimiter: new RegExp(
+      `(:(?:${getEscapedKeys(annotations)}):|${getEscapedKeys(
+        emoticons
+      )}|${specialEmoticonsRegex})`,
+      "g"
+    ),
     dict: assign(annotations, emoticons, specialEmoticons)
   };
 
   const emojiWithoutEmoticons = {
-    delimiter: new RegExp(`(:(?:${getEscapedKeys(annotations)}):)`, 'g'),
+    delimiter: new RegExp(`(:(?:${getEscapedKeys(annotations)}):)`, "g"),
     dict: annotations
   };
 
   let buildImageUrl = (hex, options) => {
     if (options.host) {
-      return compact([options.host, options.path, `${hex}.${options.ext}`]).join('/');
-    } else if (options.emojiType === 'twemoji') {
-      return `https://twemoji.maxcdn.com/${options.ext}/${hex}.${options.ext}`;
-    } else if (options.emojiType === 'emojione') {
-      return `http://cdn.jsdelivr.net/emojione/assets/${options.ext}/${hex.toUpperCase()}.${options.ext}`;
+      return compact([
+        options.host,
+        options.path,
+        `${hex}.${options.ext}`
+      ]).join("/");
+    } else if (options.emojiType === "twemoji") {
+      return `https://files.dosomegood.ca/twemoji/${options.ext}/${hex}.${
+        options.ext
+      }`;
+    } else if (options.emojiType === "emojione") {
+      return `http://cdn.jsdelivr.net/emojione/assets/${
+        options.ext
+      }/${hex.toUpperCase()}.${options.ext}`;
     } else {
-      throw new Error('Invalid emojiType is passed');
+      throw new Error("Invalid emojiType is passed");
     }
   };
 
-  let getKey = (key) => {
+  let getKey = key => {
     if (key.match(/^:.*:$/)) {
-      return key.replace(/^:/, '').replace(/:$/, '');
+      return key.replace(/^:/, "").replace(/:$/, "");
     } else {
       return key;
     }
   };
 
   let emojifyTextToSingleEmoji = (text, options) => {
-    let { dict } = options.useEmoticon ? emojiWithEmoticons : emojiWithoutEmoticons;
+    let { dict } = options.useEmoticon
+      ? emojiWithEmoticons
+      : emojiWithoutEmoticons;
     let hex = dict[getKey(text)];
-    if (!!options.strict && !hex) throw new Error(`Could not find emoji: ${text}.`);
+    if (!!options.strict && !hex)
+      throw new Error(`Could not find emoji: ${text}.`);
     if (!hex) return text;
     return React.createElement(
-      'img',
+      "img",
       assign(options.attributes, {
         src: buildImageUrl(hex, options)
       })
@@ -74,16 +93,19 @@ let ReactEmoji = () => {
   };
 
   let emojifyText = (text, options) => {
-    let { delimiter, dict } = options.useEmoticon ? emojiWithEmoticons : emojiWithoutEmoticons;
+    let { delimiter, dict } = options.useEmoticon
+      ? emojiWithEmoticons
+      : emojiWithoutEmoticons;
     return compact(
       text.split(delimiter).map(function(word, index) {
         let match = word.match(delimiter);
-        if (!!options.strict && word !== '' && match === null) throw new Error(`Could not find emoji: ${word}.`);
+        if (!!options.strict && word !== "" && match === null)
+          throw new Error(`Could not find emoji: ${word}.`);
         if (match) {
           let hex = dict[getKey(match[0])];
           if (hex === null) return word;
           return React.createElement(
-            'img',
+            "img",
             assign(options.attributes, {
               key: index,
               src: buildImageUrl(hex, options)
@@ -105,7 +127,7 @@ let ReactEmoji = () => {
       } else {
         return emojifyText(text, options);
       }
-    },
+    }
   };
 };
 
